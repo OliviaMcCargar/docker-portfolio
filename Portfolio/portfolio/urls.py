2759ac463711ba30d4f68d853e030ae5c653bdc8
Olivia <oliviamccargar@gmail.com>
2022-08-28 14:42:14 -0400

    Changed view to portfolio, made simple, unstylized view

--- Portfolio/portfolio/urls.py
+++ Portfolio/portfolio/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /project/5/
+    path('<int:project_id>/', views.detail, name='detail'),
+    # ex: /project/5/description/
+    path('<int:project_id>/description/', views.results, name='description'),
 ]
--- Portfolio/portfolio_site/urls.py
+++ Portfolio/portfolio_site/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('portfolio/', include('portfolio.urls')),
-    path('test/', admin.site.urls),
+    path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
 ]
